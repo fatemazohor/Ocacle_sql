06cf0be31cf4b3d562e1a06e10fd85b80d304cc2
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-07-15 13:04:26 +0600

    add_15.07.23

---
+++ HR16.sql
@@ -0,0 +1,23 @@
+select
+    EID,
+    ENAME,
+    E_ID,
+    E_NAME
+from
+    EMP_1277608 A8
+    left outer join EMP_1277609 A9 on ( A8.EID = A9.E_ID );
+
+SELECT 'Dear Customer'||' '||first_name||',' FROM employees;
+
+
+SELECT 'Dear Customer' ||  first_name  || ',' FROM employees;
+
+drop table emp_888 PURGE;
+
+FLASHBACK table hr.emp_888 to BEFORE DROP;
+/* hjdhf
+dfhd
+dfhdfj
+djfkdj*/
+
+REM
