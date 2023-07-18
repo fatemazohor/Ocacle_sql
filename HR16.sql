feebc6a91c7f5d1fe0ec1e81790c83fbef464d18
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-07-18 11:06:26 +0600

    add_18/07/23

--- HR16.sql
+++ HR16.sql
@@ -1,11 +1,11 @@
-select
-    EID,
-    ENAME,
-    E_ID,
-    E_NAME
-from
-    EMP_1277608 A8
-    left outer join EMP_1277609 A9 on ( A8.EID = A9.E_ID );
+SELECT
+    eid,
+    ename,
+    e_id,
+    e_name
+FROM
+    emp_1277608 l
+    LEFT OUTER JOIN emp_1277609 r ON ( a8.eid = a9.e_id );
 
 SELECT 'Dear Customer'||' '||first_name||',' FROM employees;
 
@@ -21,3 +21,24 @@ dfhdfj
 djfkdj*/
 
 REM
+
+ALTER TABLE emp_777
+Modify e_name constraint emp_777_nNull Not null;
+
+ALTER TABLE emp_777
+Modify constraint emp_778_nNull Not null (e_dob
+);
+
+CREATE VIEW emp34 AS
+    SELECT
+        department_id,
+        AVG(salary) average,
+        MAX(salary) maxsalary
+    FROM
+        employees
+    GROUP BY
+        department_id
+    ORDER BY
+        department_id;
+
+select * from emp34;
